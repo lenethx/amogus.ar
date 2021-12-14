bdac1fefa4bff7683e4724d739bc00e3f716f758
lenethx <lenethxcv@gmail.com>
2021-12-14 16:33:10 -0600

    initial commit

---
+++ 404.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+
+		<title>ඞඞඞඞඞඞඞඞඞඞඞ</title>
+		<link rel="icon" href="Amogus1.png">
+		<link rel="stylesheet" href="200b.css">
+	</head>
+	<body>
+	
+	error 404: demasiado drip
+	
+	</body>
+</html>
