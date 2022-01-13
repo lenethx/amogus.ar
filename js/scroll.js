f3cbcfe0cab71d1c2200920a9ab62cf32c2e5aef
lenethx <lenethxcv@gmail.com>
2022-01-12 19:26:39 -0600

    fully functionig parallax, excluding small problem in high res

--- js/scroll.js
+++ js/scroll.js
@@ -2,36 +2,41 @@ function applytransform(layers, beforestr, topDistance, ){
   for (let layer of layers){
 	
 	let depth = layer.getAttribute('data-depth');
-	let movement = (-(topDistance * depth));
+	let movement = (-((topDistance/window.innerWidth*800) * depth));
 	let a='';
 	let defernum=0;
 	beforestr.slice(0,-1).forEach(x=>{
 		//console.log();
 		if (((typeof x === 'string' || x instanceof String)) && x.substr(0,5)=='defer'){
 			defernum=x.substr(5);
-			console.log('being deferred')
+			//console.log('being deferred')
 		} else if (defernum !=0){
 			if(topDistance > window.innerWidth*1){
 				a+=x+(-((topDistance-defernum*window.innerWidth/100) * depth));
-				console.log(`deferred from ${x+movement} to ${x+(-((topDistance-defernum*window.innerWidth/100) * depth))}`)
+				//console.log(`deferred from ${x+movement} to ${x+(-((topDistance-defernum*window.innerWidth/100) * depth))}`)
 			} else{
 				a+=x+0;
-				console.log(`deferred from ${x+movement} to ${x+0}`)
+				//console.log(`deferred from ${x+movement} to ${x+0}`)
 			}
 			defernum=0;
 		} else {
 			a+=x+movement;
 		}
 	});
-	a+=beforestr.at(-1)
+	a+=beforestr.at(-1);
 
 	layer.style['-webkit-transform'] = a;
 	layer.style['-moz-transform'] = a;
 	layer.style['-ms-transform'] = a;
 	layer.style['-o-transform'] = a;
 	layer.style.transform = a;
-	console.log(a);
+	//console.log(a);
   }
+}
+
+function setContentHeight(){
+	document.getElementById('abscontent').style.height=`calc( ${document.getElementById('content').offsetHeight}px + 150vw )`;
+	//console.log(`calc( ${document.getElementById('content').offsetHeight}px + 50vw )`)
 }
 
 
@@ -41,8 +46,9 @@ window.addEventListener('scroll', function(event) {
 	layers = document.getElementsByClassName("parallax");
 	hlayers = document.getElementsByClassName("hor-parallax");
 	impostor=document.getElementsByClassName("impostor");
-	applytransform(layers, ['defer100','translate(0, calc(','vw / 22))'], topDistance)
-	applytransform(hlayers, ['translate(calc(','defer100','vw / -19), calc(','vh / 22)) '], topDistance);
-	applytransform(impostor, ['translate(calc(calc(max(','vw, ','defer100','vh ) / -1) - '+(window.matchMedia("(max-width: 768px)").matches ? '15vw':'8vw')+'), calc(','vh / 4)) rotate(calc(','deg * 2.5))'], topDistance);  
+	applytransform(layers, ['defer100','translate(0, calc(','vw / 22))'], topDistance);
+	applytransform(hlayers, ['translate(calc(','defer100','vw / -19), calc(','vw / 44)) '], topDistance);
+	applytransform(impostor, ['translate(calc(calc(','defer100','vw  / -1.3) - '+(window.matchMedia("(max-width: 768px)").matches ? '10vw':'8vw')+'), calc(','vw / 8)) rotate(calc(','deg * 2.5))'], topDistance);  
 });  
-
+setContentHeight();
+window.addEventListener('resize', setContentHeight);
--- views/index.php
+++ views/index.php
@@ -11,8 +11,40 @@
 
   </head>
   <body>
-    <div class="stickbox">
-      <div class="abs100">
+  <div id="abscontent">
+      <div id="content">
+        <div class="container">
+          <section class="first-section">
+            <div class="row">
+              <div class="col-sm-6">
+                <h1>Amogus.</h1>
+              </div>
+            </div>
+      
+            <div class="row">
+              <div class="col-sm-6">
+                <p>“The most detestable wickedness, the most horrid cruelties, and the greatest miseries, that have afflicted the human race have had their origin in this thing called revelation, or revealed religion. It has been the most dishonourable belief against the character of the divinity, the most destructive to morality, and the peace and happiness of man, that ever was propagated since man began to exist. It is better, far better, that we admitted, if it were possible, a thousand devils to roam at large, and to preach publicly the doctrine of devils, if there were any such, than that we permitted one such impostor and monster as Moses, Joshua, Samuel, and the Bible prophets, to come with the pretended word of God in his mouth, and have credit among us.”</p>
+                <p class="qauthor">― Thomas Paine, The Age of Reason</p>
+                <p>“If we could believe that he [Jesus] really countenanced the follies, the falsehoods, and the charlatanism which his biographers [Gospels] father on him, and admit the misconstructions, interpolations, and theorizations of the fathers of the early, and the fanatics of the latter ages, the conclusion would be irresistible by every sound mind that he was an impostor”</p>
+                <p class="qauthor">― Thomas Jefferson, Letters of Thomas Jefferson</p>
+              </div>
+              <div class="col-sm-6">
+                <p>“Don’t you ever dare judge, for who among us can say that when the devil himself offered us a deal, we refused?”</p>
+                <p class="qauthor">― Kamand Kojouri</p>
+                <p>“This Fremen religious adaptation, then, is the source of what we now recognize as "The Pillars of the Universe," whose Qizara Tafwid are among us all with signs and proofs and prophecy.”</p>
+                <p class="qauthor">― Princess Irulan, Arrakis Awakening</p>
+                <a class="tutorial-link" href="https://medium.com/@PatrykZabielski/how-to-make-multi-layered-parallax-illustration-with-css-javascript-2b56883c3f27">
+                  Learn how to create this parallax effect</a>
+              </div>
+            </div>
+          </section>
+          <div class="clarification">A lot of these quotes are religious, but I'm not trying to make any point. I just found them while looking for impostor quotes.</div>
+        </div>
+      
+      </div>
+    </div>
+    <div class="stickbox150">
+      <div class="abs150">
         <div class="layer-4 layer " ></div>
       </div>
       <div id="hero">
@@ -30,36 +62,7 @@
       </div>
       
     </div>
-    <div id="content">
-      <div class="container">
-        <section class="first-section">
-          <div class="row">
-            <div class="col-sm-6">
-              <h1>Amogus.</h1>
-            </div>
-          </div>
-          
-          <div class="row">
-            <div class="col-sm-6">
-              <p>“The most detestable wickedness, the most horrid cruelties, and the greatest miseries, that have afflicted the human race have had their origin in this thing called revelation, or revealed religion. It has been the most dishonourable belief against the character of the divinity, the most destructive to morality, and the peace and happiness of man, that ever was propagated since man began to exist. It is better, far better, that we admitted, if it were possible, a thousand devils to roam at large, and to preach publicly the doctrine of devils, if there were any such, than that we permitted one such impostor and monster as Moses, Joshua, Samuel, and the Bible prophets, to come with the pretended word of God in his mouth, and have credit among us.”</p>
-              <p class="qauthor">― Thomas Paine, The Age of Reason</p>
-              <p>“If we could believe that he [Jesus] really countenanced the follies, the falsehoods, and the charlatanism which his biographers [Gospels] father on him, and admit the misconstructions, interpolations, and theorizations of the fathers of the early, and the fanatics of the latter ages, the conclusion would be irresistible by every sound mind that he was an impostor”</p>
-              <p class="qauthor">― Thomas Jefferson, Letters of Thomas Jefferson</p>
-            </div>
-            <div class="col-sm-6">
-              <p>“Don’t you ever dare judge, for who among us can say that when the devil himself offered us a deal, we refused?”</p>
-              <p class="qauthor">― Kamand Kojouri</p>
-              <p>“This Fremen religious adaptation, then, is the source of what we now recognize as "The Pillars of the Universe," whose Qizara Tafwid are among us all with signs and proofs and prophecy.”</p>
-              <p class="qauthor">― Princess Irulan, Arrakis Awakening</p>
-              <a class="tutorial-link" href="https://medium.com/@PatrykZabielski/how-to-make-multi-layered-parallax-illustration-with-css-javascript-2b56883c3f27">
-                Learn how to create this parallax effect</a>
-            </div>
-          </div>
-        </section>
-        <div class="clarification">A lot of these quotes are religious, but I'm not trying to make any point. I just found them while looking for impostor quotes.</div>
-      </div>
-      
-    </div>
+    
 
   </body>
 </html>
